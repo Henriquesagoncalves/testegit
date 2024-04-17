2674b9a9eceb52b188485d75af0949aa66720e14
HenriqueAraujo <henrique12971@gmail.com>
2024-04-16 22:26:59 -0300

    segundo commit

--- Cartorio.c
+++ Cartorio.c
@@ -5,36 +5,45 @@
 int main()
 {
 	int opcao=0;//Definindo variáveis
+	int x=1;
 	
-	setlocale(LC_ALL, "Portuguese");//Definindo a linguagem
-	
-	printf("###Cartório da EBAC###\n\n");//início do menu
-	printf("Escolha a opção desejada do menu:\n\n");
-	printf("\t1 - Registrar nomes\n");
-	printf("\t2 - Consultar nomes\n");
-	printf("\t3 - Deletar nomes\n\n");
-	printf("Opção: ");//fim do menu
-	
-	scanf("%d", &opcao);//armazenando a escolha do usuário
-	
-	system("cls");
-	
-	if(opcao==1)//início da seleção
+	for (x=1;x=1;)
 	{
-		printf("Você escolheu o registro de nomes!");
+		system("cls");
+		setlocale(LC_ALL, "Portuguese");//Definindo a linguagem
+	
+		printf("###Cartório da EBAC###\n\n");//início do menu
+		printf("Escolha a opção desejada do menu:\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n\n");
+		printf("Opção: ");//fim do menu
+	
+		scanf("%d", &opcao);//armazenando a escolha do usuário
+	
+		system("cls");
+	
+		if(opcao==1)//início da seleção
+		{
+		printf("Você escolheu o registro de nomes!\n");
+		system("pause");
+		}
+		if(opcao==2)
+		{
+		printf("Você escolheu consultar os nomes!\n");
+		system("pause");
+		}
+		if(opcao==3)
+		{
+		printf("Você escolheu deletar nomes!\n");
+		system("pause");
+		}
+		if(opcao>=4 || opcao<=0)
+		{
+		printf("Esta opção não está disponível!\n");
+		system("pause");
+		}//fim da seleção
 	}
-	if(opcao==2)
-	{
-		printf("Você escolheu consultar os nomes!");
-	}
-	if(opcao==3)
-	{
-		printf("Você escolheu deletar nomes!");
-	}
-	if(opcao>=4 || opcao<=0)
-	{
-		printf("Esta opção não está disponível!");
-	}//fim da seleção
 }
 	
 
