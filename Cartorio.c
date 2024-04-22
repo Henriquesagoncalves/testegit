265038e9b319ca4c410ad10bf59507f9079a1c7a
HenriqueAraujo <henrique12971@gmail.com>
2024-04-22 11:08:24 -0300

    adicionando funçoes

--- Cartorio.c
+++ Cartorio.c
@@ -2,14 +2,35 @@
 #include <stdlib.h> //biblioteca de alocação de espaço em memória
 #include <locale.h> //biblioteca de alocações de texto por regiões
 
+int registro()
+{
+	printf("Você escolheu o registro de nomes!\n");
+	system("pause");
+}
+
+int consulta()
+
+{
+	printf("Você escolheu consultar os nomes!\n");
+	system("pause");	
+}
+
+int deletar()
+{
+	printf("Você escolheu deletar nomes!\n");
+	system("pause");
+}
+
 int main()
 {
 	int opcao=0;//Definindo variáveis
-	int x=1;
+	int laco=1;
 	
-	for (x=1;x=1;)
+	for (laco=1;laco=1;)
 	{
+	
 		system("cls");
+		
 		setlocale(LC_ALL, "Portuguese");//Definindo a linguagem
 	
 		printf("###Cartório da EBAC###\n\n");//início do menu
@@ -22,28 +43,28 @@ int main()
 		scanf("%d", &opcao);//armazenando a escolha do usuário
 	
 		system("cls");
+		
+		switch(opcao)
+		{
+			case 1:
+			registro();
+			break;
+			
+			case 2:
+			consulta();
+			break;
+			
+			case 3:
+			deletar();
+			break;
+			
+			default:
+			printf("Esta opção não está disponível!\n");
+			system("pause");
+			break;
+				
+		}
 	
-		if(opcao==1)//início da seleção
-		{
-		printf("Você escolheu o registro de nomes!\n");
-		system("pause");
-		}
-		if(opcao==2)
-		{
-		printf("Você escolheu consultar os nomes!\n");
-		system("pause");
-		}
-		if(opcao==3)
-		{
-		printf("Você escolheu deletar nomes!\n");
-		system("pause");
-		}
-		if(opcao>=4 || opcao<=0)
-		{
-		printf("Esta opção não está disponível!\n");
-		system("pause");
-		}//fim da seleção
-	}
+		
 }
-	
-
+}
