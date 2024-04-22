fa8a611cf98965271fc1cb7097a930147c6a9fd9
HenriqueAraujo <henrique12971@gmail.com>
2024-04-22 14:43:56 -0300

    finalizando adicionar e consultar

--- Cartorio.c
+++ Cartorio.c
@@ -1,18 +1,90 @@
 #include <stdio.h> //biblioteca de comunicação com o usuário
 #include <stdlib.h> //biblioteca de alocação de espaço em memória
 #include <locale.h> //biblioteca de alocações de texto por regiões
+#include <string.h> //biblioteca responssável por cuidar das strings
 
 int registro()
 {
-	printf("Você escolheu o registro de nomes!\n");
+	
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo [40];
+	
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s", cpf);
+	
+	strcpy(arquivo, cpf);//Responsavel por copiar os valores das string
+	
+	FILE *file; //cria o arquivo
+	file = fopen(arquivo, "w");//cria o arquivo
+	fprintf(file,cpf);//salva o valor da variavel
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s", nome);
+
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s", sobrenome);
+
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s", cargo);
+
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);
+	
 	system("pause");
 }
 
 int consulta()
 
 {
-	printf("Você escolheu consultar os nomes!\n");
-	system("pause");	
+	setlocale(LC_ALL, "Portuguese");
+	
+	char cpf[40];
+	char conteudo[200];
+	
+	printf("Digite o CPF a ser consultado: ");
+	scanf("%s", cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("Não foi possìvel abrir o arquivo, não localizado!.\n");
+	}
+	
+	while(fgets(conteudo, 200, file) != NULL)
+	{
+		printf("\nEssas são as informações do usuário: ");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	system("pause");
 }
 
 int deletar()
